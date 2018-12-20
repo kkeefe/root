bae0ceaf766508a8e1c9fa25af70769325e1c7ea
Kevin Keefe <kevinpk@hawaii.edu>
2018-12-19 19:30:03 -1000

    updated changes to the readme.org file, as well as included an integral example..

--- intro_file_graphs/graphTest.C
+++ intro_file_graphs/graphTest.C
@@ -11,22 +11,27 @@
   // f1->Draw();
   // f1->SetTitle("Title of the Graph; Y-Axis name (unit); X-axis name (unit)");
 
-
-  // //this is the information that is set for the first formula
-  // c1->cd(1);
-  // f1 = new TF1("f", "10*(1-x/2.4)^2", 0, 2.4);
-  // f1->SetLineColor(kGreen -3);
-  // f1->Draw();
-  // f1->SetTitle("I_d vs V_gs; V_gs (-V); I_d (mA)");
-
-  //feel free the play with additional examples of 1-D functions you'd like to see:
+  //this is the information that is set for the first formula
   c1->cd(1);
-  // lets try something like a wein displacement function.. (inspired via qual prep)
-  f1 = new TF1("f", "x^2/(e^x-1)", 0, 10);
+  f1 = new TF1("f", "10*(1-x/2.4)^2", 0, 2.4);
   f1->SetLineColor(kGreen -3);
   f1->Draw();
-  f1->SetTitle("Probability Density of a Photon; energy (w); U(w) (w)");
-  
-  
+  f1->SetTitle("I_d vs V_gs; V_gs (-V); I_d (mA)");
+
+  // //feel free the play with additional examples of 1-D functions you'd like to see:
+  // c1->cd(1);
+  // // lets try something like a wein displacement function.. (inspired via qual prep)
+  // f1 = new TF1("f", "x^3/(e^x-1)", 0, 10);
+  // f1->SetLineColor(kGreen -3);
+  // f1->Draw();
+  // f1->SetTitle("Probability Density of a Photon; energy (w); U(w) (w)");
+
+  // current errors here.. there is something wrong with root trying to parse the Log function..
+  // //Helmholtz free energy of a photon gas
+  // c1->cd(1);
+  // f1 = new TF1("f", "x^2*TMath::LogE(1-e^(x))", 0, 10);
+  // f1->SetLineColor(kGreen -3);
+  // f1->Draw();
+  // f1->SetTitle("Probability Density of a Photon; energy (w); U(w) (w)");
 
 }
--- intro_file_graphs/hello.C
+++ intro_file_graphs/hello.C
@@ -1,3 +1,19 @@
 {
+  //this program still works like any C program you would expect
   cout << "hello world" << endl;
+
+  //this allows for multiple plots to be put into the same macro
+  c1 = new TCanvas("c1", "Canvas name", 500, 500);
+
+  //this is the information that is set for the first formula
+  c1->cd(1);
+  f1 = new TF1("f", "sin(x)/x", -10, 10);
+  f1->SetLineColor(kGreen -3);
+  f1->Draw();
+  f1->SetTitle("graph title; y (y); x (x)");
+
+  //the integral function takes only two parameters of the function you're integrating over..
+  double int_f1 = f1->Integral(-10, 10);  
+  cout << "value of the integral is: " << int_f1 << endl;
+  
 }
