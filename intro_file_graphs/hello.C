9df216a7e301fc42f67b24ee8fc4fa28d7af0236
Kevin Keefe <kevinpk@hawaii.edu>
2018-12-20 15:27:51 -1000

    updated changes to the basic hello.C file and included a new integral example..

--- intro_file_graphs/hello.C
+++ intro_file_graphs/hello.C
@@ -15,5 +15,18 @@
   //the integral function takes only two parameters of the function you're integrating over..
   double int_f1 = f1->Integral(-10, 10);  
   cout << "value of the integral is: " << int_f1 << endl;
-  
+
+
+//   //part of 7.65 in textbook: thermal physics by Schroeder
+//   //this is the information that is set for the first formula
+//   c1->cd(1);
+//   f1 = new TF1("f", "sqrt(x)/(e^x-1)", 0, 100);
+//   f1->SetLineColor(kGreen -3);
+//   f1->Draw();
+//   f1->SetTitle("practice problem; y (y); x (x)");
+
+//   //the integral function takes only two parameters of the function you're inte grating over..
+//   double int_f1 = f1->Integral(0, 10000);  //keep in mind that the root integrals will not like domains extending beyond 10,000 in the positive direction..
+//   cout << "value of the integral is: " << int_f1 << endl;
+
 }
