0d6f477526bf37381786b5bfbbe72e9f0455969b
kkeefe <kevinpk@hawaii.edu>
2019-04-02 11:03:05 -1000

    updated for fill_hist_from_file

--- intro_file_hist/hist_test/fill_hist_from_file.cpp
+++ intro_file_hist/hist_test/fill_hist_from_file.cpp
@@ -97,7 +97,8 @@ void residual_fits(TGraph *tg, TF1 *fit, double_t x_min, double_t x_max, TGraph 
   for (int i = 0; i < tg->GetN(); i++)
   {
     difference = y_points[i] - fit->Eval(x_points[i]);
-    if (difference != 0 and x_points[i] > x_min and x_points[i] < x_max) //make sure that you're only looking in the assigned range..
+    // if (difference != 0 and x_points[i] > x_min and x_points[i] < x_max) //make sure that you're only looking in the assigned range..
+    if (x_points[i] > x_min and x_points[i] < x_max) //make sure that you're only looking in the assigned range..
     {
       tg_out->SetPoint(++(*point), x_points[i], difference); //save each point to graph the differences later..
     }
@@ -105,15 +106,19 @@ void residual_fits(TGraph *tg, TF1 *fit, double_t x_min, double_t x_max, TGraph 
 }
 
 //defines the range of the total fits, number of total fits, and the fitting function
-void iterate_fit(TGraph *tg, double_t x_min, double_t x_max, int num_fits, TGraph *tg_out, int *point)
+TGraph* iterate_fit(TGraph *tg, double_t x_min, double_t x_max, int num_fits, TGraph *tg_out, int *point)
 {
   //select choice of fitting function here..
   TF1 *my_func = new TF1("my_func", "gaus");
+  TGraph *tg_ret = new TGraph(); //create the graph of the slope you wish to return..  
+  int point_ret(0);
 
   double_t width = (x_max - x_min) / num_fits; //define the width of where the fit will be.
   //previous parameters to guess for fits (avoid explosive fits)
   double_t prev_p0, prev_p1, prev_p2, prev_p3;
   vector<pair<double_t, double_t>> thresholds_xy;
+
+
 
   for (int i = 0; i < num_fits; ++i)
   {
@@ -130,23 +135,27 @@ void iterate_fit(TGraph *tg, double_t x_min, double_t x_max, int num_fits, TGrap
     //save the max difference from each of the fits..
 
     residual_fits(tg, my_func, x_min_p, x_max_p, tg_out, point);
-
+    //here to calculate a graph for the slopes of the values..
+    
     prev_p0 = my_func->GetParameter("Constant");
     prev_p1 = my_func->GetParameter("Mean");
     prev_p1 = my_func->GetParameter("Sigma");
+    
+    tg_ret->SetPoint(point_ret++, (x_min_p + x_max_p)/2, prev_p1);
 
-    // cout << "constant value: " << prev_p0
-    //      << "\t Mean value: " << prev_p1
-    //      << "\t Slope value: " << prev_p2
-    //      << "\t range: " << x_min_p << " to " << x_max_p << endl;
+    cout << "constant value: " << prev_p0
+         << "\t Mean value: " << prev_p1
+         << "\t Slope value: " << prev_p2
+         << "\t range: " << x_min_p << " to " << x_max_p << endl;
   }
+  return tg_ret;
 }
 
-set<int> Register_Set =
-    {111, 113, 115, 109, 85, 117, 119, 121, 112, 114, 116, 77, 124, 118, 120, 122, 110, 82, 80, 79, 89, 87, 90, 92, 83, 78, 81, 84, 88, 86, 91, 123, 19, 47, 49, 48, 58, 56, 54, 59, 46, 51, 50, 45, 57, 55, 53, 22, 18, 16, 14, 20, 60, 28, 26, 24, 17, 15, 13, 52, 21, 27, 25, 23};
-
 // set<int> Register_Set =
-//     {56};
+//     {111, 113, 115, 109, 85, 117, 119, 121, 112, 114, 116, 77, 124, 118, 120, 122, 110, 82, 80, 79, 89, 87, 90, 92, 83, 78, 81, 84, 88, 86, 91, 123, 19, 47, 49, 48, 58, 56, 54, 59, 46, 51, 50, 45, 57, 55, 53, 22, 18, 16, 14, 20, 60, 28, 26, 24, 17, 15, 13, 52, 21, 27, 25, 23};
+
+set<int> Register_Set =
+    {92};
 
 // need to convert register information from the Lyso data to the noise measurement on the chanell..
 // KEEP IN MIND that channel here actually refers to the pixel number minus 1!!
@@ -372,7 +381,7 @@ void fill_hist_from_file(const char *file)
       cout << "found a fill at: " << Reg_I_Want << endl;
       TGraph *tg_diff = new TGraph();
       //this method fills the tg_diff graph..
-      iterate_fit(g1, 2900, 3700, 96, tg_diff, &point2);
+      TGraph* fit_graph = iterate_fit(g1, 2900, 3700, 96, tg_diff, &point2);
       pair<Double_t, double_t> threshold_xy, slope_diff_xy, slope_noise;
       double_t threshold_val(250); //define a threshold value to look for in the difference graph..
       threshold_xy = find_thresh_and_diff_of_tg_diff(tg_diff, threshold_val);
@@ -383,8 +392,8 @@ void fill_hist_from_file(const char *file)
 
       c1->Divide(3, 1);
       c1->cd(1);
-      construct_graph(g1, "graph_1", "threshold", "Scalar_counts", 2900, 3700);
-      construct_graph(g0, "graph_0", "threshold", "Scalar_counts", 2900, 3700);
+      construct_graph(g1, "graph_1", "threshold", "Scalar_counts", 3050, 3450);
+      construct_graph(g0, "graph_0", "threshold", "Scalar_counts", 3050, 3450);
       g0->SetTitle("Noise_data");
       g1->SetTitle("Source_data");
       c1->GetPad(1)->SetLogy();
@@ -432,11 +441,13 @@ void fill_hist_from_file(const char *file)
       legend->AddEntry(tl, "Gaus Fit");
       legend->AddEntry(tl2, "Linear Fit");
       legend->AddEntry(tl3, "Point-slope Fit");
-      legend->DrawClone("Same");
+      // legend->DrawClone("Same");
+      legend->Draw("");
 
       c1->cd(2);
       construct_graph_min(tg_diff, "graph_2", "threshold", "Difference", 2900, 3700);
       tg_diff->Draw("APL");
+      
       TPaveText *labeA = new TPaveText(0.15, 0.70, 0.4, 0.79, "brNDC");
       labeA->AddText(make_constChar(threshold_xy.first));
       labeA->Draw();
@@ -448,19 +459,31 @@ void fill_hist_from_file(const char *file)
       labeB2->Draw();
       tl->DrawLine(threshold_xy.first, tg_diff->GetMinimum(), threshold_xy.first, tg_diff->GetMaximum());
 
+      // c1->cd(3);
+      // construct_graph_min(g2, "graph_3", "threshold", "scalar point-slope", 2900, 3700);
+      // g2->Draw("APL");
+      // TPaveText *labeC = new TPaveText(0.15, 0.70, 0.4, 0.79, "brNDC");
+      // labeC->AddText(make_constChar(slope_diff_xy.first));
+      // labeC->Draw();
+      // TPaveText *labeD = new TPaveText(0.15, 0.80, 0.40, 0.89, "brNDC");
+      // labeD->AddText(make_constChar(slope_diff_xy.second));
+      // labeD->Draw();
+      // TPaveText *labeB3 = new TPaveText(0.15, 0.20, 0.4, 0.29, "brNDC");
+      // labeB3->AddText("linear-fit");
+      // labeB3->Draw();
+      // tl2->DrawLine(slope_diff_xy.first, g2->GetMinimum(), slope_diff_xy.first, g2->GetMaximum());
+
       c1->cd(3);
-      construct_graph_min(g2, "graph_3", "threshold", "scalar point-slope", 2900, 3700);
-      g2->Draw("APL");
-      TPaveText *labeC = new TPaveText(0.15, 0.70, 0.4, 0.79, "brNDC");
-      labeC->AddText(make_constChar(slope_diff_xy.first));
-      labeC->Draw();
-      TPaveText *labeD = new TPaveText(0.15, 0.80, 0.40, 0.89, "brNDC");
-      labeD->AddText(make_constChar(slope_diff_xy.second));
-      labeD->Draw();
-      TPaveText *labeB3 = new TPaveText(0.15, 0.20, 0.4, 0.29, "brNDC");
-      labeB3->AddText("linear-fit");
-      labeB3->Draw();
-      tl2->DrawLine(slope_diff_xy.first, g2->GetMinimum(), slope_diff_xy.first, g2->GetMaximum());
+      //construct_graph(fit_graph, "graph_fit", "threshold", "constant_value", 3050, 3450);
+      fit_graph->SetFillColor(kGreen);
+      fit_graph->SetTitle("fit_graph");
+      fit_graph->GetXaxis()->SetTitle("threshold");
+      fit_graph->GetXaxis()->SetRangeUser(2900,3650);
+      fit_graph->SetMaximum(5000);
+      fit_graph->SetMinimum(-5000);
+      c1->GetPad(3)->SetLogy(1);
+      fit_graph->GetYaxis()->SetTitle("constant_value");
+      fit_graph->Draw("ABQ");
 
       //begin saving the file stuff you want to save..
       string file_name(file);
@@ -479,6 +502,7 @@ void fill_hist_from_file(const char *file)
       output_txt << (conv_Reg_to_pixel(Reg_I_Want) + 1) << "\t" << threshold_xy.first << "\t" << threshold_xy.second << "\t"
                  << slope_diff_xy.first << "\t" << slope_diff_xy.second
                  << "\t" << slope_noise.first << endl;
+
     }
   }
   output_txt.close(); //make sure this puppy is outside of that for loop, file will be rewritten every time..
