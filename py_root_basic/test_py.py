d60827b6a8162f367988a4042da9ee6ea1c53f9a
kkeefe <kevinpk@hawaii.edu>
2020-05-26 21:18:14 -1000

    no need for test.py

--- py_root_basic/test_py.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-# the thing we need for pyRoot to work..
-import ROOT
-
-# lets try to get our header..
-ROOT.gROOT.ProcessLine('#import "data_headers.hh"')
-
-# then lets try to make that object
